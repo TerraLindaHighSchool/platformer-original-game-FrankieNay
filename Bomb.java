a50a74bb878c5558dac183a2b5037098320e7de5
FrankieNay <89489233+FrankieNay@users.noreply.github.com>
2021-09-27 15:39:37 -0700

    Activity 3 Complete

---
+++ Bomb.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class bomb here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bomb extends Obstacle
+{
+        private float yVelocity;
+    private final float GRAVITY;
+
+    public void act()
+    {
+
+    }
+
+    protected void fall()
+    { 
+
+    }
+
+        public Bomb(float gravity)
+    {
+        GRAVITY = gravity;
+    }
+}
--- Level1.java
+++ Level1.java
@@ -16,5 +16,30 @@ public class Level1 extends World
     {    
         // Create a new world with 1200x800 cells with a cell size of 1x1 pixels.
         super(1200, 800, 1, false); 
+        setPaintOrder(Player.class, Platform.class, Obstacle.class, Collectable.class,
+            Door.class, HUD.class);
+        prepare();
     }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Door door = new Door();
+        addObject(door,1175,42);
+        Player player = new Player();
+        addObject(player,427,57);
+        Floor floor = new Floor();
+        addObject(floor,600,800);
+        player.setLocation(1364,261);
+
+        addObject(new Bomb(GRAVITY), 1050, 765);
+
+        Player player2 = new Player();
+        addObject(player2,24,760);
+    }
+        private final float GRAVITY = 0.0667f;
+    private final GreenfootSound MUSIC = new GreenfootSound("zapsplat_024.mp3");
 }
